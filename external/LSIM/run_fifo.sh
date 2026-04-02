3a10c2aecba18a36c090275db079ddac90c6777a
Ubuntu <jou32@wisc.edu>
2026-04-02 22:42:42 +0000

    fix a small issue in run_fifo.sh

--- external/LSIM/run_fifo.sh
+++ external/LSIM/run_fifo.sh
@@ -9,7 +9,7 @@ file_array=("c432" "c499" "c880" "c1355" "c1908" "c2670" "c3540" "c5315" "c6288"
 echo "--- Starting Test all C circuits (FIFO) ---"
 for file in "${file_array[@]}"; do
     echo "Processing: ${file}"
-    ./main/MA_cuda_fifo "../benchmark/${file}.ckt" "../benchmark/${file}.flst" "../benchmark/${file}.ptn"
+    ./main/MA_cuda_partition_fifo "../benchmark/${file}.ckt" "../benchmark/${file}.flst" "../benchmark/${file}.ptn"
     echo "Finished ${file}"
     echo "--------------------------------"
 done
